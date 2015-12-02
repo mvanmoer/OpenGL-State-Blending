151f4ed62c31021262e7d677b2fadcb6a260e980
Mark Van Moer <mvanmoer@illinois.edu>
2015-12-02 10:24:44 -0600

    Added surface formating for OpenGL 4.2, multisampling, etc.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSurfaceFormat>
+#include <QOpenGLContext>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // This stuff is the analogue of glutInit*
+    // I'm assuming some things based on my particular workstation driver
+    QSurfaceFormat fmt;
+    fmt.setDepthBufferSize(24);
+    fmt.setSamples(32);
+    fmt.setVersion(4,2);
+    fmt.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(fmt);
+
     MainWindow w;
     w.show();
 
