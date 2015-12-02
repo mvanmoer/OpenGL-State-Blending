f09fbf46812e266fdbe81ff014b7a567cc1d7fab
Mark Van Moer <mvanmoer@illinois.edu>
2015-12-02 10:26:09 -0600

    All of the GUI signals/slots connections.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QColorDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,3 +13,122 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+// Alpha sliders
+void MainWindow::on_clear_color_alpha_slider_valueChanged(int value)
+{
+    ui->myGLCanvas->setClearAlpha(float(value)/100.0f);
+}
+
+void MainWindow::on_top_alpha_slider_valueChanged(int value)
+{
+    ui->myGLCanvas->setTopAlpha(float(value)/100.0f);
+}
+
+void MainWindow::on_const_color_alpha_slider_valueChanged(int value)
+{
+    ui->myGLCanvas->setBlendAlpha(float(value)/100.0f);
+}
+
+void MainWindow::on_middle_alpha_slider_valueChanged(int value)
+{
+    ui->myGLCanvas->setMiddleAlpha(float(value)/100.0f);
+}
+
+void MainWindow::on_bottom_alpha_slider_valueChanged(int value)
+{
+    ui->myGLCanvas->setBottomAlpha(float(value)/100.0f);
+}
+
+// checkboxes
+void MainWindow::on_blend_checkbox_toggled(bool checked)
+{
+    ui->myGLCanvas->setEnableBlending(checked);
+}
+
+void MainWindow::on_depth_test_checkbox_toggled(bool checked)
+{
+    ui->myGLCanvas->setEnableDepthTest(checked);
+}
+
+void MainWindow::on_colorLogic_checkbox_toggled(bool checked)
+{
+    ui->myGLCanvas->setEnableColorLogicOp(checked);
+}
+
+void MainWindow::on_separate_checkbox_toggled(bool checked)
+{
+    ui->myGLCanvas->setEnabledSeparateBlending(checked);
+}
+
+// Dropdown menus
+void MainWindow::on_srcFactorOpts_activated(const QString &arg1)
+{
+   ui->myGLCanvas->setSrcFactor(arg1);
+}
+
+void MainWindow::on_dstFactorOps_activated(const QString &arg1)
+{
+    ui->myGLCanvas->setDstFactor(arg1);
+}
+
+void MainWindow::on_sepSrcFactorOps_activated(const QString &arg1)
+{
+    ui->myGLCanvas->setSeparateSrcFactor(arg1);
+}
+
+void MainWindow::on_blendEqOps_activated(const QString &arg1)
+{
+    ui->myGLCanvas->setBlendEquation(arg1);
+}
+
+void MainWindow::on_logicOpOps_activated(const QString &arg1)
+{
+    ui->myGLCanvas->setLogicOp(arg1);
+}
+
+void MainWindow::on_sepDstFactorOps_activated(const QString &arg1)
+{
+    ui->myGLCanvas->setSeparateDstFactor(arg1);
+}
+
+// Color dialogs
+void MainWindow::on_glClearColorButton_clicked()
+{
+    QColor selectedColor = QColorDialog::getColor(Qt::white,this,"glClearColor");
+    ui->myGLCanvas->setClearColor(selectedColor);
+}
+
+void MainWindow::on_topColorButton_clicked()
+{
+    QColor selectedColor = QColorDialog::getColor(Qt::white, this, "top sprite color");
+    ui->myGLCanvas->setTopColor(selectedColor);
+}
+
+void MainWindow::on_middleColorButton_clicked()
+{
+    QColor selectedColor = QColorDialog::getColor(Qt::white, this, "middle sprite color");
+    ui->myGLCanvas->setMiddleColor(selectedColor);
+}
+
+void MainWindow::on_bottomColorButton_clicked()
+{
+    QColor selectedColor = QColorDialog::getColor(Qt::white, this, "bottom sprite color");
+    ui->myGLCanvas->setBottomColor(selectedColor);
+}
+
+void MainWindow::on_blendColorButton_clicked()
+{
+    QColor selectedColor = QColorDialog::getColor(Qt::white, this, "glBlendColor");
+    ui->myGLCanvas->setBlendColor(selectedColor);
+}
+
+
+
+
+
+
+
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,48 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_top_alpha_slider_valueChanged(int value);
+
+    void on_blend_checkbox_toggled(bool checked);
+
+    void on_depth_test_checkbox_toggled(bool checked);
+
+    void on_srcFactorOpts_activated(const QString &arg1);
+
+    void on_dstFactorOps_activated(const QString &arg1);
+
+    void on_sepSrcFactorOps_activated(const QString &arg1);
+
+    void on_blendEqOps_activated(const QString &arg1);
+
+    void on_logicOpOps_activated(const QString &arg1);
+
+    void on_glClearColorButton_clicked();
+
+    void on_topColorButton_clicked();
+
+    void on_middleColorButton_clicked();
+
+    void on_bottomColorButton_clicked();
+
+    void on_blendColorButton_clicked();
+
+    void on_clear_color_alpha_slider_valueChanged(int value);
+
+    void on_sepDstFactorOps_activated(const QString &arg1);
+
+    void on_const_color_alpha_slider_valueChanged(int value);
+
+    void on_colorLogic_checkbox_toggled(bool checked);
+
+    void on_middle_alpha_slider_valueChanged(int value);
+
+    void on_bottom_alpha_slider_valueChanged(int value);
+
+    void on_separate_checkbox_toggled(bool checked);
+
 private:
     Ui::MainWindow *ui;
 };
